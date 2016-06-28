f5e8c7f43be0d618f1ebc2eda5645596f230890a
Karl Stoney <kstoney@thoughtworks.com>
2016-06-28 23:29:10 +0100

    RabbitMQ based pubsub, topic exchange with filters

--- test/messageBrokers.js
+++ test/messageBrokers.js
@@ -30,17 +30,6 @@ describe('Brokers', () => {
         });
       });
 
-      it('Should not allow multiple subscriptions to the same channel (for now)', (done) => {
-        broker.subscribe('test', (message) => {
-          should(message).eql('test message'); 
-          done();     
-        }, () => { 
-          broker.publish('test', 'test message', (err) => {
-            should.ifError(err);
-          });    
-        });
-      });
-
       it('Should handle a JSON message', (done) => {
         broker.subscribe('test', (message) => {
           should(message).eql({ data: 'test' }); 
