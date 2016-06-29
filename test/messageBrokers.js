fe060d678d49a303d5cc183ab01f2e9b43d7d8e0
Karl Stoney <kstoney@thoughtworks.com>
2016-06-29 17:34:24 +0100

    Implemented worker queues

--- test/messageBrokers.js
+++ test/messageBrokers.js
@@ -21,22 +21,22 @@ describe('Brokers', () => {
       });
 
       it('Should publish to a given channel', (done) => {
-        broker.subscribe('test', (message) => {
+        broker.subscribe('filter.test.givenchannel', (message) => {
           should(message).eql('test message'); 
           done();     
         }, () => { 
-          broker.publish('test', 'test message', (err) => {
+          broker.publish('filter.test.givenchannel', 'test message', (err) => {
             should.ifError(err);
           });    
         });
       });
 
       it('Should handle a JSON message', (done) => {
-        broker.subscribe('test', (message) => {
+        broker.subscribe('filter.test.json', (message) => {
           should(message).eql({ data: 'test' }); 
           done();     
         }, () => {
-          broker.publish('test', { data: 'test' }, (err) => {
+          broker.publish('filter.test.json', { data: 'test' }, (err) => {
             should.ifError(err);
           });    
         });
@@ -44,30 +44,104 @@ describe('Brokers', () => {
 
       it('a message sent to a different channel shouldnt appear on my subscription', (done) => {
         let setupGoodChannel = (next) => {
-          broker.subscribe('good-channel', () => {
+          broker.subscribe('filter.test.diff.chan1', () => {
             done();
           }, next);
         };
 
         let setupBadChannel = (next) => {
-          broker.subscribe('bad-channel', () => {
+          broker.subscribe('filter.test.diff.chan2', () => {
             done(new Error('Message was receieved on the incorrect channel!'));
           }, next);
         };
 
         let sendMessage = (err) => {
           if(err) { return done(err); }
-          broker.publish('good-channel', 'test message', (err) => {
+          broker.publish('filter.test.diff.chan1', 'test message', (err) => {
             should.ifError(err);
           });   
         };
 
         async.series([
-          setupGoodChannel,
-          setupBadChannel
+            setupGoodChannel,
+            setupBadChannel
         ], sendMessage);
-
       });
+
+      it('messages should be received by all subscribers', (done) => {
+        let count = 0;
+        let gotMessage = () => {
+          count++;
+          if(count === 2) {
+            done();
+          }
+        };
+
+        let setupChannel = (next) => {
+          broker.subscribe('filter.test.all', gotMessage, next);
+        };
+
+        let setupAnotherChannel = (next) => {
+          broker.subscribe('filter.test.all', gotMessage, next);
+        };
+
+        let sendMessage = (err) => {
+          if(err) { return done(err); }
+          broker.publish('filter.test.all', 'test message', (err) => {
+            should.ifError(err);
+          });   
+        };
+
+        async.series([
+            setupChannel,
+            setupAnotherChannel
+        ], sendMessage);
+      });
+
+      it('messages on worker subscriptions should be receieved by one', (done) => {
+        let count = 0;
+        let gotMessage = () => {
+          count++;
+          if(count === 2) {
+            done(new Error('Too many messages were recieved!'));
+          } else {
+            done();
+          }
+        };
+
+        let setupChannel = (next) => {
+          broker.subscribeWorker('filter.test.worker', gotMessage, next);
+        };
+
+        let setupAnotherChannel = (next) => {
+          broker.subscribeWorker('filter.test.worker', gotMessage, next);
+        };
+
+        let sendMessage = (err) => {
+          if(err) { return done(err); }
+          broker.publish('filter.test.worker', 'test message', (err) => {
+            should.ifError(err);
+          });   
+        };
+
+        async.series([
+            setupChannel,
+            setupAnotherChannel
+        ], sendMessage);
+      });
+
+      it('messages already receieved shouldnt be receieved again on reconnect', (done) => {
+        broker.subscribeWorker('filter.testingReconnect', () => {
+          broker.reset(() => {
+            broker.subscribeWorker('filter.testingReconnect', () => {
+              done(new Error('Got another message when we shouldnt have!'));
+            }, done);
+          });
+        }, () => {
+          broker.publish('filter.testingReconnect', 'test', () => {});
+        });
+      });
+
     });
   };
 
